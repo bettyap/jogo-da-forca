b8b497b21f58d0f137d7dee97ed2f6379b4e65f0
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-04-03 16:16:59 -0300

    feat: create component Word

---
+++ src/components/Word.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="word-container">
+
+    <div class="word-letters">
+      <div class="word-letter" v-for="(letter,key) in word" :key="key">
+        {{ (verificarLetra(letter) || stage === "enforcado" ) ? letter : '' }}
+      </div>
+    </div>
+
+    <div class="word-tip">
+      {{tip}}
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    word: String,
+    tip: String,
+    verificarLetra: Function,
+    stage: String
+  },
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .word-container{
+    text-transform: uppercase;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .word-letters {
+    display: flex;
+    margin-bottom: 1.25rem;
+  }
+  .word-letter {
+    width: 1.875rem;
+    height: 1.875rem;
+    margin: 0 0.3125rem;
+    border-bottom: 1px solid blue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+</style>
