4e1aee401219c332f6d9b623b42c58ed92e51ea4
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-04-04 08:48:56 -0300

    feat: create component gibbet

---
+++ src/components/Gibbet.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="gibbet-container">
+    <img 
+      :src="`https://raw.githubusercontent.com/william-costa/wdev-hangman-game-resources/master/images/hangman/${errors}.svg`"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    errors: Number
+  },
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .forms-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 1.25rem;
+  }
+  .gibbet-container img {
+    height: 6.25rem;
+  }
+</style>
