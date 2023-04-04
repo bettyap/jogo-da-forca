06fbe1f6f157780ad3e0d5346a58cf8252cf5887
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-04-04 08:49:31 -0300

    feat: create component finale

---
+++ src/components/Finale.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="finale-container">
+    <div :class="`finale-text ${stage}`">
+      {{text}}
+    </div>
+    <button 
+      @click="jogarNovamente"
+      class="finale-button"
+    >
+      Jogar Novamente
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    stage: String,
+    text: String,
+    jogarNovamente: Function
+  },
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<style scoped>
+  .finale-text {
+    margin: 1.25rem 0;
+    font-size: 1.75rem;
+    font-weight: bold;
+  }
+  .finale-text.ganhador {
+    color: greenyellow;
+  }
+  .finale-text.enforcado {
+    color: red;
+  }
+  .keyboard-container{
+    width: auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    padding: 1rem;
+  }
+  .keyboard-button {
+    margin: 0.25rem;
+    width: 3.28125rem;
+    border-radius: 0.5rem;
+    text-transform: uppercase;
+  }
+  
+</style>
