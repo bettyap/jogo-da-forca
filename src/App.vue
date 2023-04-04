a6b30ac6cf1e642e0cdfa8243007fb3dbaa5f521
Elisabeth Aparecida <elisabeth.silva@carambola.com.vc>
2023-04-04 08:51:00 -0300

    feat: create app, adjustment style

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,34 @@
   <div class="container">
     <h1> Jogo da forca</h1>
     <section v-if="screen === 'inicio'">
-      <Forms />
+
+      <Forms v-if="stage === 'palavra'"
+        title="Defina a palavra"
+        button="Próximo"
+        :action="setPalavra"
+      />
+
+      <Forms v-else-if="stage === 'dica'"
+        title="Defina a dica"
+        button="Iniciar jogo =)"
+        :action="setDica"
+      />
 
     </section>
 
     <section v-else-if="screen === 'jogo'">
-      Jogo
+
+      <Game 
+        :errors="errors"
+        :word="word"
+        :tip="tip"
+        :verificarLetra="verificarLetra"
+        :stage="stage"
+        :letters="letters"
+        :jogar="jogar"
+        :jogarNovamente="jogarNovamente"
+      />
+
     </section>
   </div>
   
@@ -15,15 +37,69 @@
 
 <script>
 import Forms from './components/Forms.vue'
+import Game from './components/Game.vue'
 
   export default {
     data() {
       return {
-        screen: 'inicio'
+        screen: 'inicio',
+        stage: 'palavra',
+        word: '',
+        tip: '',
+        errors: 0,
+        letters: []
       }
     },
-    components: { Forms }
+    components: { 
+      Forms, 
+      Game
+    },
+    methods: {
+      setPalavra(palavra) {
+        this.word = palavra;
+        this.stage = 'dica'
+      },
+      setDica(dica) {
+        this.tip = dica;
+        this.screen = 'jogo'
+        this.stage = 'jogo'
+      },
+      verificarLetra(letter) {
+        return this.letters.find(item => item.toLowerCase() === letter.toLowerCase());
+      },
+      jogar(letter) {
+        this.letters.push(letter);
+
+        this.verificarErros(letter)
+      },
+      verificarErros(letter) {
+        if(this.word.toLowerCase().indexOf(letter.toLowerCase()) >= 0 ){
+          return this.verificarAcertos();
+        }
+
+        this.errors++;
+
+        if(this.errors === 6){
+          this.stage = 'enforcado'
+        }
+      },
+      verificarAcertos() {
+        let letrasUnicas = [...new Set(this.word.split(''))];
+        if(letrasUnicas.length === (this.letters.length - this.errors)){
+          this.stage = 'ganhador'
+        }
+      },
+      jogarNovamente() {
+        this.word = '';
+        this.tip = '';
+        this.errors = 0;
+        this.letters = [];
+        this.screen = 'inicio';
+        this.stage = 'palavra';
+      }
+    }
   }
+
 </script>
 
 <style scoped>
